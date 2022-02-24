cbc7eead5505b450416d0d1297f47c729c9a1439
Tayyab Mughal <tayyabmughal676@gmail.com>
2022-02-24 17:59:45 +0500

    Added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
-Version:0.0.1
+Version: 0.0.3
+  Added: TextButton
+Version:0.0.2
+  Added: Text
+  
--- lib/thor_text_pkg.dart
+++ lib/thor_text_pkg.dart
@@ -40,3 +40,33 @@ class ThorText extends StatelessWidget {
     );
   }
 }
+
+class ThorTextButton extends StatelessWidget {
+  final onPressed;
+  final Widget child;
+  final ButtonStyle? style;
+
+  const ThorTextButton({
+    Key? key,
+    required this.child,
+    required this.onPressed,
+    this.style,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: child,
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.all(16.0),
+        primary: Colors.white,
+        backgroundColor: Colors.blue,
+        elevation: 9.0,
+        textStyle: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
