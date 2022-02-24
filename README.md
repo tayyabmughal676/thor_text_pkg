4cbc9b08aedaae1c12a5fcfef310e48cb559160d
Tayyab Mughal <tayyabmughal676@gmail.com>
2022-02-24 17:49:33 +0500

    Added

--- README.md
+++ README.md
@@ -1,27 +1,18 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
+# Thor Text Packages
+It is just simple text package return custom text.
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+## Authors
+- [@Thor](https://www.github.com/tayyabmughal676)
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+-
+
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+-
 
 ## Usage
 
--- lib/thor_text_pkg.dart
+++ lib/thor_text_pkg.dart
@@ -2,16 +2,41 @@ library thor_text_pkg;
 
 import 'package:flutter/material.dart';
 
-class ThorText extends StatefulWidget {
-  const ThorText({Key? key}) : super(key: key);
+class ThorText extends StatelessWidget {
+  final String text;
+  final TextAlign? textAlign;
+  final Color? color;
+  final FontWeight? fontWeight;
+  final int? maxLines;
+  final String? fontFamily;
+  final bool? softWrap;
+  final double? fontSize;
 
-  @override
-  createState() => _ThorText();
-}
+  const ThorText(
+      {Key? key,
+      required this.text,
+      this.textAlign = TextAlign.start,
+      this.color,
+      this.fontWeight,
+      this.maxLines,
+      this.fontFamily,
+      this.softWrap,
+      this.fontSize})
+      : super(key: key);
 
-class _ThorText extends State<ThorText> {
   @override
   Widget build(BuildContext context) {
-    return const Text("ThorText");
+    return Text(
+      text,
+      textAlign: textAlign,
+      softWrap: softWrap,
+      maxLines: maxLines,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontFamily: fontFamily,
+        fontWeight: fontWeight,
+        color: color,
+      ),
+    );
   }
 }
