8998a84017fd021bcfb18cece1d981925c465e85
Tayyab Mughal <tayyabmughal676@gmail.com>
2022-02-24 17:25:04 +0500

    Initial commit

---
+++ lib/thor_text_pkg.dart
@@ -0,0 +1,17 @@
+library thor_text_pkg;
+
+import 'package:flutter/material.dart';
+
+class ThorText extends StatefulWidget {
+  const ThorText({Key? key}) : super(key: key);
+
+  @override
+  createState() => _ThorText();
+}
+
+class _ThorText extends State<ThorText> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("ThorText");
+  }
+}
