68b6fb23197bfbecde7d88e15ae131152207d55c
tayyabmughal676 <tayyabmughal676@gmail.com>
2025-07-30 16:36:19 +0500

    Removed GitHub Actions workflow for publishing to pub.dev.
    The workflow file `.github/workflows/publish.yml` was deleted. This workflow was responsible for publishing the package to pub.dev on new version tags.
    
    Additionally, commented-out test code in `test/thor_text_pkg_test.dart` was removed.

--- test/thor_text_pkg_test.dart
+++ test/thor_text_pkg_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:thor_text_pkg/thor_text_pkg.dart';
 
 void main() {
-
   // test('adds one to input values', () {
   //
   //   final ThorText = ThorText();
